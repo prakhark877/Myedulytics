b266174adc29f060eb43b3e332b565e5b6e4f4f7
rdhakad <rdhakad@flair-solution.com>
2025-02-12 12:37:49 +0530

    new changes s3

--- resources/views/dashboard/admin/quizzes/create.blade.php
+++ resources/views/dashboard/admin/quizzes/create.blade.php
@@ -79,39 +79,71 @@
 
 
     <script src="/js/aws-sdk-2.553.0.min.js"></script>
-{{-- <script src="/js/common.js"></script> --}}
-
-<script>
-    var s3;
-var identity_id = "ap-south-1:6cfbe6e5-6132-4178-9059-bfcf1655c107";
-var identity_pool_id = "ap-south-1:6cfbe6e5-6132-4178-9059-bfcf1655c107";
-var private_bucket = "littleedventure";
-var public_bucket = "littleedventure";
-var cloudfront_url = "https://d2vmtwtvjnckox.cloudfront.net";
-var s3_bucket_region = "ap-south-1";
-
-
-
-        AWS.config.region = s3_bucket_region; // Region
-        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-                    IdentityPoolId: identity_pool_id,
-                    IdentityId: identity_id,
-                    Logins: {
-                        "littleedventure": 'super@admin.com',
-                    },
-                });
-        AWS.config.apiVersions = {
-                    s3: "2006-03-01",
-                };
-        s3 = new AWS.S3({
-            httpOptions: {
-                connectTimeout: 1000 * 1000, // time succeed in starting the call
-                timeout: 1000 * 1000, // time to wait for a response
-                // the aws-sdk defaults to automatically retrying
-                // if one of these limits are met.
-            },
-        });
-
+    <script type="text/javascript">
+        var s3;
+          var identity_id = "";
+          var token = "";
+          var identity_pool_id = "";
+          var private_bucket = "";
+          var public_bucket = "";
+          var cloudfront_url = "";
+          var s3_bucket_region = "";
+          $(document).ready(function () {
+              getS3Details();
+          });
+          function getS3Details() {
+              //  debugger;
+              $.ajax({
+                  type: "GET",
+                  async: true,
+                  ContentDisposition: "attachment",
+                  url:   "/s3/token",
+                  // url: "https://api.segwik.com/api/s3/cloudUrl/",
+                  cache: false,
+                  success: function (data) {
+                      var d = JSON.parse(data);
+                      //debugger;
+                      if (d.success == false) {
+                          alert("token error " + d.message);
+                          return false;
+                      }
+                      identity_id = d.identity_id;
+                      token = d.token;
+                      identity_pool_id = d.identity_pool_id;
+                      private_bucket = d.private_bucket;
+                      public_bucket = d.public_bucket;
+                      cloudfront_url = d.cloudfront_url;
+                      s3_bucket_region = "ap-south-1";
+                      AWS.config.region = s3_bucket_region; // Region
+                      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                          IdentityPoolId: identity_pool_id,
+                          IdentityId: identity_id,
+                          Logins: {
+                              "cognito-identity.amazonaws.com": token,
+                          },
+                      });
+                      AWS.config.apiVersions = {
+                          s3: "2006-03-01",
+                      };
+                      s3 = new AWS.S3({
+                          httpOptions: {
+                              connectTimeout: 1000 * 1000, // time succeed in starting the call
+                              timeout: 1000 * 1000, // time to wait for a response
+                              // the aws-sdk defaults to automatically retrying
+                              // if one of these limits are met.
+                          },
+                      });
+                  },
+                  error: function (error) {
+                      //   debugger;
+                      alert("Error token : ", error);
+                      return false;
+                      console.log(error.statusText);
+                  },
+              });
+          }
+       
+      
 
     
     function uploadS3File(fileUploaderid) {
