1404d29c21913abed9336b1eeb98761c2340d5df
rdhakad <rdhakad@flair-solution.com>
2025-02-13 10:46:25 +0530

    new changes in segwik

---
+++ config/constants.php
@@ -0,0 +1,20 @@
+<?php
+
+use Carbon\Carbon;
+
+return [
+    'AWS_CREDENTIALS' => [
+        'IDENTITYPOOLID' => "ap-south-1:6cfbe6e5-6132-4178-9059-bfcf1655c107",
+        'TOKENDURATION' => 86400,
+        //1440,
+        'PROVIDERNAME' => 'littleedventure',
+        'S3BUCKET' => [
+            'PUBLIC' => "littleedvanture",
+            'PRIVATE' => "littleedvanture",
+            'REGION' => "ap-south-1"
+        ],
+        'CLOUDFRONTURL' => "https://d2vmtwtvjnckox.cloudfront.net",
+        'REGION' => "ap-south-1"
+    ]
+        
+];
