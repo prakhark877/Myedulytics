9b5427508a02686d5a5fb566a45c431554678765
rdhakad <rdhakad@flair-solution.com>
2025-02-25 18:42:32 +0530

    fix age group

--- resources/views/dashboard/admin/quizzes/index.blade.php
+++ resources/views/dashboard/admin/quizzes/index.blade.php
@@ -43,7 +43,12 @@
                                         <td>{{ $quiz->category->cat_title }}</td>
                                         <td>{{ $quiz->subcategory->subcat_title }}</td>
                                         <td>{{ $selectedAgeGroup['name'] ?? 'N/A' }}</td>
-                                        <td><img src="{{ config('constants.AWS_CREDENTIALS.CLOUDFRONTURL') .$quiz->image }}" alt="Current File" width="50" height="50"></td>
+                                        <td>@if($quiz->image)
+                                            <img src="{{ config('constants.AWS_CREDENTIALS.CLOUDFRONTURL') .$quiz->image }}" alt="Current File" width="50" height="50">
+                                        @else
+                                        N/A
+                                        @endif
+                                        </td>
                                         <td>
                                             <a href="{{ route('quizzes.edit', $quiz->id) }}"
                                                 class="btn btn-warning">Edit</a>
